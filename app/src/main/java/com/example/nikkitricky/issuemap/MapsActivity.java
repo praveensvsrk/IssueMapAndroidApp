59cada44f3711799e25df048e6b10e5af0ce6393
nikkitricky <nikhith432@gmail.com>
2018-06-02 16:12:30 +0530

    your here

--- app/src/main/java/com/example/nikkitricky/issuemap/MapsActivity.java
+++ app/src/main/java/com/example/nikkitricky/issuemap/MapsActivity.java
@@ -1,6 +1,13 @@
 package com.example.nikkitricky.issuemap;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Toast;
@@ -12,9 +19,11 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
+    private LocationManager lm;
+    Double lon, lat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,13 +49,47 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(17.42972116, 78.34088064);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Microsoft"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = lm.getLastKnownLocation(lm.NETWORK_PROVIDER);
+        onLocationChanged(location);
+
+        LatLng cur_place = new LatLng(lat, lon);
+        mMap.addMarker(new MarkerOptions().position(cur_place).title("Your Here"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(cur_place));
     }
 
     public void newissue(View v){
         Toast.makeText(this,"New issue",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+         lat = location.getLatitude();
+        lon = location.getLongitude();
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
     }
 }
